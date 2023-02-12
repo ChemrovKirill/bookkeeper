993690b93eafcb40ea53b650c544cfde5bfe0f92
ChemrovKirill <chemrov.ks@phystech.edu>
2023-02-12 16:49:50 +0300

    Finish crud for SQL repo

--- bookkeeper/repository/sqlite_repository.py
+++ bookkeeper/repository/sqlite_repository.py
@@ -14,6 +14,7 @@ class SQLiteRepository(AbstractRepository[T]):
         self.table_name = cls.__name__.lower()
         self.fields = get_annotations(cls, eval_str=True)
         self.fields.pop('pk')
+        self.obj_cls = cls
         # with sqlite3.connect(self.db_file) as con:
         #     cur = con.cursor()
         #     names = ', '.join(self.fields.keys())
@@ -21,9 +22,11 @@ class SQLiteRepository(AbstractRepository[T]):
         #     con.close()
         
     def add(self, obj: T) -> int:
+        if getattr(obj, 'pk', None) != 0:
+            raise ValueError(f'trying to add object {obj} with filled `pk` attribute')
         names = ', '.join(self.fields.keys())
         p = ', '.join("?" * len(self.fields))
-        values = [getattr(obj, x) for x in self.fields]
+        values = [getattr(obj, f) for f in self.fields]
         with sqlite3.connect(self.db_file) as con:
             cur = con.cursor()
             cur.execute('PRAGMA foreign_keys = ON')
@@ -35,8 +38,21 @@ class SQLiteRepository(AbstractRepository[T]):
         con.close()
         return obj.pk
 
-    def get(self, pk):
-        ...
+    def get(self, pk: int) -> T | None:
+        with sqlite3.connect(self.db_file) as con:
+            cur = con.cursor()
+            res_row = cur.execute(
+                f'SELECT * FROM {self.table_name} '
+                + f'WHERE ROWID=={pk}'
+            ).fetchone()
+        con.close()
+        if res_row is None:
+            return None
+        res_obj = self.obj_cls()
+        for field, value in zip(self.fields, res_row):
+            setattr(res_obj, field, value)
+        res_obj.pk = pk
+        return res_obj
 
     def get_all(self, where: dict[str, Any] | None = None) -> list[T]:
         """
@@ -46,8 +62,27 @@ class SQLiteRepository(AbstractRepository[T]):
         """
 
     def update(self, obj: T) -> None:
-        """ Обновить данные об объекте. Объект должен содержать поле pk. """
+        fields = ", ".join([f"{f}=?" for f in self.fields.keys()])
+        values = [getattr(obj, f) for f in self.fields]
+        with sqlite3.connect(self.db_file) as con:
+            cur = con.cursor()
+            cur.execute(
+                f'UPDATE {self.table_name} SET {fields} '
+                + f'WHERE ROWID=={obj.pk}',
+                values
+            )
+            if cur.rowcount == 0:
+                raise ValueError('attempt to update object with unknown primary key')
+        con.close()
 
     def delete(self, pk: int) -> None:
-        """ Удалить запись """
+        with sqlite3.connect(self.db_file) as con:
+            cur = con.cursor()
+            cur.execute(
+                f'DELETE FROM  {self.table_name} '
+                + f'WHERE ROWID=={pk}'
+            )
+            if cur.rowcount == 0:
+                raise ValueError('attempt to delete object with unknown primary key')
+        con.close()
     
--- tests/test_repository/test_sqlite_repository.py
+++ tests/test_repository/test_sqlite_repository.py
@@ -7,7 +7,7 @@ import pytest
 def custom_class():
     class Custom():
         pk: int = 0
-        f1: int = 10
+        f1: int = 0
         f2_name: str = "f2_value"
     return Custom
 
@@ -16,9 +16,60 @@ def custom_class():
 def repo(custom_class):
     return SQLiteRepository(db_file="database/test_sqlrepo.db", cls=type(custom_class()))
 
-def test_crud(repo, custom_class):
-    obj = custom_class()
-    pk = repo.add(obj)
-    assert obj.pk == pk
-    # TODO: test update and delete
 
+def test_crud(repo, custom_class):
+    # create
+    obj_add = custom_class()
+    obj_add.f1 = 1
+    obj_add.f2_name = "test_crud"
+    pk = repo.add(obj_add)
+    assert pk == obj_add.pk
+    # read
+    obj_get = repo.get(pk)
+    assert obj_get is not None
+    assert obj_get.pk == obj_add.pk
+    assert obj_get.f1 == obj_add.f1
+    assert obj_get.f2_name == obj_add.f2_name
+    # update
+    obj_upd = custom_class()
+    obj_upd.pk = pk
+    obj_upd.f1 = 1
+    obj_upd.f2_name = "test_crud_upd"
+    repo.update(obj_upd)
+    obj_get = repo.get(pk)
+    assert obj_get.pk == obj_upd.pk
+    assert obj_get.f1 == obj_upd.f1
+    assert obj_get.f2_name == obj_upd.f2_name
+    # delete
+    repo.delete(pk)
+    assert repo.get(pk) is None
+
+
+def test_cannot_add_with_pk(repo, custom_class):
+    obj = custom_class()
+    obj.pk = 1
+    with pytest.raises(ValueError):
+        repo.add(obj)
+
+
+def test_cannot_add_without_pk(repo):
+    with pytest.raises(ValueError):
+        repo.add(0)
+
+
+def test_cannot_update_without_pk(repo, custom_class):
+    obj = custom_class()
+    with pytest.raises(ValueError):
+        repo.update(obj)
+
+
+def test_cannot_update_unexistent(repo, custom_class):
+    obj = custom_class()
+    obj.pk = -1
+    with pytest.raises(ValueError):
+        repo.update(obj)
+
+
+def test_cannot_update_unexistent(repo, custom_class):
+    with pytest.raises(ValueError):
+        repo.delete(-1)
