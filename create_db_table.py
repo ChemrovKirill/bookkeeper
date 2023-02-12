c897d9d3902d1792ebe0440b0a3eccef794a5d22
ChemrovKirill <chemrov.ks@phystech.edu>
2023-02-12 21:12:41 +0300

    mv create_db_table.py

---
+++ create_db_table.py
@@ -0,0 +1,15 @@
+import sqlite3
+from inspect import get_annotations
+from bookkeeper.models.category import Category
+from bookkeeper.models.expense import Expense
+
+
+db_file = "database/simple-client-sql.db"
+for cls in [Category, Expense]:
+    table_name = cls.__name__.lower()
+    fields = get_annotations(cls, eval_str=True)
+    fields.pop('pk')
+    with sqlite3.connect(db_file) as con:
+        cur = con.cursor()
+        cur.execute(f"CREATE TABLE {table_name}({', '.join(fields.keys())})")
+    con.close()
