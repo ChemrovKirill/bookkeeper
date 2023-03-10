bb39fc6f61eff6be602c6132d7bfd0893ee2804a
Кирилл Чемров <chemrovsk@mail.ru>
2023-03-10 03:44:56 +0300

    expenses cell change test

--- tests/test_view/test_expenses.py
+++ tests/test_view/test_expenses.py
@@ -1,36 +1,40 @@
 """
 Тесты GUI для модуля с таблицей расходов
 """
-import pytest
+#import pytest
 from pytestqt.qt_compat import qt_api
+from PySide6.QtCore import Qt
 
 from bookkeeper.view.expenses import ExpensesTableWidget, ExpensesTableGroup
 
-def exp_modifier(pk, attr, new_val):
-    pass
+test_data = [["1_1","1_2","1_3","1_4",1],
+                ["2_1","2_2","2_3","2_4",2],]
+
+exp_modifier = lambda pk, attr, new_val: None
 
 def test_create_widget(qtbot):
     widget = ExpensesTableWidget(exp_modifier)
     qtbot.addWidget(widget)
 
+
 def test_add_data(qtbot):
     widget = ExpensesTableWidget(exp_modifier)
     qtbot.addWidget(widget)
-    test_data = [["1_1","1_2","1_3","1_4",1],
-                 ["2_1","2_2","2_3","2_4",2],]
     widget.add_data(test_data)
     assert widget.data == test_data
     for i, row in enumerate(test_data):
         for j, x in enumerate(row[:-1]):
             assert widget.item(i, j).text() == test_data[i][j]
 
-def test_cell_select(qtbot):
+def test_cell_changed(qtbot):
+    def exp_modifier(pk, attr, new_val):
+        exp_modifier.was_called = True
+        assert pk == test_data[0][4]
+        assert new_val == test_data[0][0]
+    exp_modifier.was_called = False
     widget = ExpensesTableWidget(exp_modifier)
     qtbot.addWidget(widget)
-    widget.show()
-    qtbot.mouseClick(
-        widget.item(1,1),
-        qt_api.QtCore.Qt.MouseButton.LeftButton
-        )
-    qtbot.stop()
-    assert widget.selectedItems()[0] == widget.item(1,1)
+    widget.add_data(test_data)
+    widget.cellDoubleClicked.emit(0,0)
+    widget.cellChanged.emit(0,0)
+    assert exp_modifier.was_called == True
